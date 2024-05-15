2eecba335890b8ee533242ce96cb1466bd4e7539
jenny-ngo-29 <jen.ngo1175@gmail.com>
2024-05-15 14:31:20 -0700

    roles

--- CS5CardsHearts.py
+++ CS5CardsHearts.py
@@ -2,13 +2,14 @@
 
 # WEEK 1
     # conditionals 
+    # Jenny+Yuan
 
 # WEEK 2
     # functions / returns / slicing
-
+    # Jenny+Yuan    
 
 # WEEK 3
-
+    # Rachel+Jonathan
 # WEEK 4
 
 # WEEK 5
@@ -16,15 +17,22 @@
 # WEEK 6
 
 # WEEK 7
+# Leena+Florence
 
 # WEEK 8
+# Florence+Jenny 
 
 # WEEK 9
+# Jonathan+Yuan
 
 # WEEK 10
+# Jenny+Yuan
 
 # WEEK 11
+# Rachel+Leena
 
 # WEEK 12
+# Jonathan+Florence
 
 # WEEK 13
+# Leena+Rachel
