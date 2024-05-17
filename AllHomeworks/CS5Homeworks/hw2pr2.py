e765b7b0d5a0333d204023963da844859264ed6c
jenny-ngo-29 <jen.ngo1175@gmail.com>
2024-05-17 13:38:48 -0700

    hw2

--- AllHomeworks/CS5Homeworks/hw2pr2.py
+++ AllHomeworks/CS5Homeworks/hw2pr2.py
@@ -55,7 +55,33 @@ def rwsteps(start, low, hi):
         time.sleep(0.05)
 
         return steps_taken
-print("Final steps taken:", rwsteps(5, 0, 10))
+    
+def poptart_race(sA, sB, PT):
+    """
+    This simulator observes two students, sA and sB, racing to reach 
+    the final poptart at the Claremont Colleges. 
+
+    sA:  the location of the first student (who wanders)
+    sB:  the location of the second student (who wanders)   
+    PT:  the location of the poptart
+
+    The poptart does not move.
+    The endpoints are always at low=0 and hi=42.  (We don't need them as inputs)
+
+    Good values to run:  poptart_race( 10, 30, 21 )  # evenly spaced
+                         poptart_race( 18, 30, 21 )  # uneven spacing: sA is much closer to 21, the poptart
+    """
+    low = 0
+    hi = 42  # Assuming the hallway is 42 units long
+
+    print("\nStart of the race:")
+    print("|" + "_" * (PT - low) + "#" + "_" * (hi - PT) + "|")
+    print(" " * sA + "A" + " " * (PT - sA - 1) + "#" + " " * (sB - PT - 1) + "B" + " " * (hi - sB))
+
+    steps = rwsteps(max(sA, sB), low, hi)
+    print("\nWinner reached the poptart in", steps, "steps!")
+poptart_race(10, 30, 21)
+#POPTART is NONFUNCTIONAL RN
 '''
 #hw2pr2 2D extra-credit
 def rs():
