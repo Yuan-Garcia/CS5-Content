5a839070a1f926daeb11cf3104f420e39f2fdaec
Yuan Garcia <yuan.garcia@gmail.com>
2024-05-20 11:46:30 -0700

    fixed the code, api call now works for pokemon

--- AllHomeworks/CS5Homeworks/hw2Dicts.py
+++ AllHomeworks/CS5Homeworks/hw2Dicts.py
@@ -2,5 +2,6 @@ import requests
 yourPokemon = "ditto"
 # check out the documentation here! 
 # https://pokeapi.co/docs/v2#pokemon-section
-response = requests.get("http://api.open-notify.org/astros")
+response = requests.get("https://pokeapi.co/api/v2/pokemon/ditto/")# + yourPokemon + "/")
 print(response.json())
+#
