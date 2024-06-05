949d47b07953c48f56dc015be51fc4fd5f5fd7c5
Yuan Garcia <yuan.garcia@gmail.com>
2024-06-04 17:13:10 -0700

    Added LOC and Comments checking + regex!

--- CodeMeasure/LOCtest.py
+++ CodeMeasure/LOCtest.py
@@ -1,2 +1,26 @@
+import re 
+
+def commentCheck(comment):
+    #hashtags = "[\#[^\n\r]+?(?:[\n\r])]"   # is the actual solution
+    hashtags = "[\#]" # is the very temporary solution until I figure out how to get the actual solution to work
+    if re.search(hashtags, comment):
+        return comment
+    else:
+        return ""
+    
+    #print(commentCheck("#will this work"))
+    #print(commentCheck("we will see"))
+    
+
+
+commentList = []
 inputfile = open("LOC.txt", "r")
-print(inputfile.read())
+for x in inputfile:
+  commentList.append(commentCheck(x))
+print("The LOC is: " + str(len(commentList)))
+commentList = [z for z in commentList if z != ""]
+
+  
+print("The comment level is: " + str(len(commentList)))
+
+#print(commentCheck(inputfile.read()))
