a63a5bac74abd8ccb0e48b835728835dcc828674
Yuan Garcia <yuan.garcia@gmail.com>
2024-06-06 10:58:07 -0700

    added Cyclomatic Complexity Function
    Added Cyclomatic complexity function, however, it is unfinished
    
    Because Cyclomatic complexity can't be checking for comments, I'm removing them too!

--- CodeMeasure/LOCtest.py
+++ CodeMeasure/LOCtest.py
@@ -1,7 +1,7 @@
 import re 
 
 def commentCheck(comment):
-    #hashtags = "[\#[^\n\r]+?(?:[\n\r])]"   # is the actual solution
+    #hashtags = "\#[^\n\r]+?(?:[\n\r])"   # is the actual solution
     hashtags = "[\#]" # is the very temporary solution until I figure out how to get the actual solution to work
     if re.search(hashtags, comment):
         return comment
@@ -9,7 +9,23 @@ def commentCheck(comment):
         return ""
     #print(commentCheck("#will this work"))
     #print(commentCheck("we will see"))
-    
+
+def CyclomaticChicanery(noCommentScriptStr):
+    addOne = "for|if|for|while|except|with|assert|Comprehension|and|or|not|implies"
+    CyclomaticCount = 0
+    for i in noCommentScriptStr:
+        if re.search(addOne, i):
+            CyclomaticCount = CyclomaticCount + 1
+    return CyclomaticCount
+
+def removeComments(fullScript):
+    multiLine = "\'\'\'[^']*\'\'\'|\"\"\"[^\"]*\"\"\""
+    fullScript = re.sub(multiLine, "", fullScript)
+    fullScript = re.sub("\#[^\n\r]+?(?:[\n\r])","",fullScript)
+    return fullScript
+
+
+
 def splitFunc(fullScript):
     scriptList = []
     tempFunc = ""
@@ -22,17 +38,25 @@ def splitFunc(fullScript):
         else:
             funcToggle = False
         if funcToggle:
-            tempFunc = tempFunc + i
+            tempFunc = tempFunc + i #the reason that this method sucks is that if people make a return line with no spaces then it calls it a new function, which no PERSON does, but LLM's do
         else:
             scriptList.append(tempFunc)
             tempFunc = ""
 
     return scriptList
 
+def determineFunc(fullScript):
+    scriptList = []
+    for i,n in enumerate(fullScript):
+        if(n.startswith("def ")):
+            scriptList.append(i)
+    return scriptList
+
 commentList = []
 totalScriptList = []
 
 inputfile = open("LOC.py", "r")
+inputfiletest2 = open("LOC.py", "r")
 for x in inputfile:
     totalScriptList.append(x)
     commentList.append(commentCheck(x))
@@ -43,7 +67,12 @@ commentList = [z for z in commentList if z != ""]
   
 print("The comment level is: " + str(len(commentList)))
 print("The percentage comments is: " + str((len(commentList)/len(totalScriptList))*100))
+print("There are " + str(len(determineFunc(totalScriptList))) + " functions present")
 #print(splitFunc(totalScriptList))
 
+# testing on LOC.py
+print(removeComments(inputfiletest2.read()))
+#print(inputfiletest2.read())
 #print(commentCheck(inputfile.read()))
+
 
