644a193d96e93b611413a6f06d6ce4fcdfb85929
Yuan Garcia <yuan.garcia@gmail.com>
2024-06-20 19:28:35 -0700

    prune chicanery

--- CodeMeasure/LOCtest.py
+++ CodeMeasure/LOCtest.py
@@ -13,15 +13,6 @@ def commentCheck(comment): #fix becauyse it works now
     #print(commentCheck("#will this work"))
     #print(commentCheck("we will see"))
 
-# def CyclomaticChicanery(noCommentScriptStr):
-#     wordScript = noCommentScriptStr.split(" ")
-#     addOne = "for|if|for|while|except|with|assert| in |and|or|not|implies" # add more list comprehensions
-#     CyclomaticCount = 0
-#     for i in wordScript:
-#         if re.search(addOne, i): # if it's in the checkers then decrement the cyclomatic count :)
-#             CyclomaticCount = CyclomaticCount + 1
-#     return CyclomaticCount
-
 def removeComments(fullScript):
     multiLine = "\'\'\'[^']*\'\'\'|\"\"\"[^\"]*\"\"\"" # gets all docstrings
     fullScript = re.sub(multiLine, "", fullScript) # replaces all docstrings with an emptystring
